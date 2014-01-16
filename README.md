38aa7df0f4d5ef4959c596cfc7efa810b979360c
RedD <sunblok@cox.net>
2014-01-16 17:20:04 +0000

    Adding first version

---
+++ README.md
@@ -0,0 +1,4 @@
+CS313
+=====
+
+This is for my CS 313 Web Development 2 Class
---
+++ demo.js
@@ -0,0 +1,6 @@
+function changeColor()
+{
+   alert("I am on loaded.");
+   var titleSpan = document.getElementById("pageTitle");
+   titleSpan.style.background:purple;"
+}
